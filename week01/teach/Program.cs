ad9fd8859731d0002bb575fdb40473e4a962aeb7
Victor Mtisi <victormtisi@gmail.com>
2026-03-12 10:42:56 +0200

    main

--- week01/teach/Program.cs
+++ week01/teach/Program.cs
@@ -9,10 +9,11 @@ Console.WriteLine("\n======================\nAlgorithms\n======================"
 Algorithms.Run();
 
 
+// Uncomment and run as you get to the solution 
 
 // Uncomment and run as you get to the solution part
 // Console.WriteLine("\n======================\nDivisors Solution\n======================");
 // DivisorsSolution.Run();
-//
+
 // Console.WriteLine("\n======================\nArray Selector Solution\n======================");
 // ArraySelectorSolution.Run();
